bb68e2769de9bb81fd783c88274348c10518d343
Osmar Baia <osmar.neto23@gmail.com>
2024-06-26 00:35:19 -0300

    Finished the requested layout

--- 3-YoutubeHomeClone/assets/scripts/main.js
+++ 3-YoutubeHomeClone/assets/scripts/main.js
@@ -1,10 +1,12 @@
 const sideMenuOpenButton = document.querySelector('#navbar-menu-btn');
 const sideMenuCloseButton = document.querySelector('#sidebar-menu-btn');
 const sideMenu = document.querySelector('#sidebar');
+const videoCardsContainer = document.querySelector('#cards-container');
 
 sideMenuOpenButton.addEventListener('click', () => {
     sideMenu.classList.toggle('hidden');
     sideMenu.classList.add('sideRightAnimation');
+    videoCardsContainer.classList.toggle('sidebar-padding')
     
     setTimeout(() => {
         sideMenu.classList.remove('sideRightAnimation');
@@ -13,9 +15,9 @@ sideMenuOpenButton.addEventListener('click', () => {
 
 sideMenuCloseButton.addEventListener("click", () => {
     sideMenu.classList.add('sideLeftAnimation');    
-    
     setTimeout(() => {
         sideMenu.classList.remove('sideLeftAnimation');
         sideMenu.classList.toggle('hidden');
+        videoCardsContainer.classList.toggle('sidebar-padding')
     }, 300);
 });
